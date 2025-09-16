9d21c1f5c6f7be8c8390df2f10d459ebe97d5689
Kévin Sanmartin <kevin.sanmartin@smart-chain.fr>
2025-09-16 22:38:03 +0200

    :sparkles: button can now get an icon

--- src/components/elements/Button/index.tsx
+++ src/components/elements/Button/index.tsx
@@ -1,4 +1,4 @@
-import { ButtonHTMLAttributes } from "react";
+import { ButtonHTMLAttributes, ReactNode } from "react";
 import classes from "./classes.module.scss";
 import classNames from "classnames";
 
@@ -10,9 +10,11 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 	size?: ButtonSize;
 	loading?: boolean;
 	fullWidth?: boolean;
+	leftIcon?: ReactNode;
+	rightIcon?: ReactNode;
 }
 
-export default function Button({ children, variant = "primary", size = "md", loading = false, fullWidth = false, className, disabled, ...props }: ButtonProps) {
+export default function Button({ children, variant = "primary", size = "md", loading = false, fullWidth = false, leftIcon, rightIcon, className, disabled, ...props }: ButtonProps) {
 	const buttonClasses = classNames(
 		classes.button,
 		classes[`variant-${variant}`],
@@ -24,7 +26,11 @@ export default function Button({ children, variant = "primary", size = "md", loa
 	return (
 		<button className={buttonClasses} disabled={disabled || loading} {...props}>
 			{loading && <span className={classes.spinner} />}
-			<span className={classes.content}>{children}</span>
+			<span className={classes.content}>
+				{leftIcon && <span className={classes.leftIcon}>{leftIcon}</span>}
+				{children}
+				{rightIcon && <span className={classes.rightIcon}>{rightIcon}</span>}
+			</span>
 		</button>
 	);
 }
